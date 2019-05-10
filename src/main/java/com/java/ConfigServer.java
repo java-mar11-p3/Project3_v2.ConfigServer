e6da15e0e620207adb73689f6abcef8e3cfc7714
poholo502 <poholo502@gmail.com>
2019-05-10 17:29:52 -0500

    adding documentation

--- src/main/java/com/java/ConfigServer.java
+++ src/main/java/com/java/ConfigServer.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
+/**
+ * 
+ * @author Poho
+ * This contains configuration files in a git repository 
+ * that could contain location and credential of our database.
+ * So that we don't have to do reconfiguration
+ */
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
